040f40449f0db407c42e9db38ee4e9b4a9bd817d
Harshit gupta <guptaharshit545@gmail.com>
2020-08-17 00:14:48 +0530

    first commit travilix

---
+++ main.js
@@ -0,0 +1,15 @@
+//Selectors
+let header = document.querySelector('.header');
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+
+window.addEventListener('scroll', function() {
+    let windowPostion = window.scrollY > 0;
+    header.classList.toggle('active', windowPostion)
+})
+
+hamburgerMenu.addEventListener('click', function () {
+    header.classList.toggle('menu-open');
+});
+
+
+console.log(header, hamburgerMenu);
